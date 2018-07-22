3e2f66b74bde98b988b47ce327ae6ee50a2df6c9
quidcain <quidcain@gmail.com>
2018-07-23 00:11:00 +0300

    Initial commit

---
+++ src/main/java/org/quidcain/jad/backend/JadBackendApplication.java
@@ -0,0 +1,12 @@
+package org.quidcain.jad.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JadBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JadBackendApplication.class, args);
+	}
+}
