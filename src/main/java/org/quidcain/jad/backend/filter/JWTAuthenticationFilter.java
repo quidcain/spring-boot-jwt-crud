b34fa0fbb9ff29454970e76008cea82ba9635d9f
quidcain <quidcain@gmail.com>
2018-07-24 02:06:35 +0300

    Change access modifiers

--- src/main/java/org/quidcain/jad/backend/filter/JWTAuthenticationFilter.java
+++ src/main/java/org/quidcain/jad/backend/filter/JWTAuthenticationFilter.java
@@ -19,10 +19,10 @@ import java.util.Collections;
 import java.util.Date;
 
 public class JWTAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
-    static final long EXPIRATION_TIME = 864_000_000; // 10 days
-    static final String SECRET = "MySecret";
-    static final String TOKEN_PREFIX = "Bearer ";
-    static final String HEADER_STRING = "Authorization";
+    public static final long EXPIRATION_TIME = 864_000_000; // 10 days
+    public static final String SECRET = "MySecret";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
 
     private AuthenticationManager authenticationManager;
 
