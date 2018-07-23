1caa0d92ab910a08ff917f6f9552cf349cc6c3cc
quidcain <quidcain@gmail.com>
2018-07-24 01:43:31 +0300

    Ignore extra json properties on login request

--- src/main/java/org/quidcain/jad/backend/model/User.java
+++ src/main/java/org/quidcain/jad/backend/model/User.java
@@ -1,11 +1,14 @@
 package org.quidcain.jad.backend.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@JsonIgnoreProperties(ignoreUnknown=true)
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
