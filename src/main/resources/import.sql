f0c5acb9cf18fb5397a182ca935b863603ca6234
quidcain <quidcain@gmail.com>
2018-07-31 00:55:41 +0300

    Add hibernate schema initialization

---
+++ src/main/resources/import.sql
@@ -0,0 +1,13 @@
+INSERT INTO user (username, password) VALUES ('Jad', '$2a$10$RiQehzM1dvoA7eYyyaG7Kunhw.BT1n0yRyVxvCa1V4AeYc3lErdNK');
+INSERT INTO user (username, password) VALUES ('Daniel', '$2a$10$RiQehzM1dvoA7eYyyaG7Kunhw.BT1n0yRyVxvCa1V4AeYc3lErdNK');
+
+INSERT INTO sample (columna, columnb, columnc, columnd, columne) VALUES ('Bail', 'McWilliams', 'Khaki', 'Ndebele', 'XS');
+INSERT INTO sample (columna, columnb, columnc, columnd, columne) VALUES ('Haven', 'Dorre', 'Green', 'Somali', 'L');
+INSERT INTO sample (columna, columnb, columnc, columnd, columne) VALUES ('Mikkel', 'Simukov', 'Turquoise', 'Gagauz', 'XL');
+INSERT INTO sample (columna, columnb, columnc, columnd, columne) VALUES ('Carmen', 'Foxon', 'Yellow', 'Icelandic', 'XL');
+INSERT INTO sample (columna, columnb, columnc, columnd, columne) VALUES ('Ali', 'Sarjent', 'Blue', 'Chinese', 'L');
+INSERT INTO sample (columna, columnb, columnc, columnd, columne) VALUES ('Kari', 'Venneur', 'Blue', 'Zulu', 'M');
+INSERT INTO sample (columna, columnb, columnc, columnd, columne) VALUES ('Clyve', 'Zmitruk', 'Orange', 'Dzongkha', 'XL');
+INSERT INTO sample (columna, columnb, columnc, columnd, columne) VALUES ('Eva', 'Doret', 'Violet', 'Belarusian', 'L');
+INSERT INTO sample (columna, columnb, columnc, columnd, columne) VALUES ('Essie', 'Ware', 'Puce', 'Amharic', '3XL');
+INSERT INTO sample (columna, columnb, columnc, columnd, columne) VALUES ('Gawain', 'Allbut', 'Maroon', 'Danish', 'M');
